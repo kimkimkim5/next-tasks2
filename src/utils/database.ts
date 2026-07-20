dd593b30a16b6a3bef74dfd0dbf107d3bac5c36c
kimkimkim5 <136133931+kimkimkim5@users.noreply.github.com>
2026-07-21 07:47:03 +0900

    DB接続ログ

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDb = async () => {
-    try {
-        console.log(process.env.DB_URI)
-        await mongoose.connect(process.env.DB_URI || '');
-    } catch {
-        console.log('DB接続に失敗しました。' );
-        throw new Error();
-    }
-}
+  try {
+    console.log("DB_URI exists:", !!process.env.DB_URI);
+
+    await mongoose.connect(process.env.DB_URI || "");
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("DB接続エラー:", error);
+    throw error;
+  }
+};
